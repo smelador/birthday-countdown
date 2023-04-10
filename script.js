ac8719049750b8d41611d6197aecf3cbf6dbf270
smelador <54758226+smelador@users.noreply.github.com>
2023-04-10 16:58:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-function calculateTimeDifference(targetDate) {
-  const now = new Date();
-  const timeDifference = targetDate - now;
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+// Set the date of your birthday
+const birthday = new Date('April 30, 2000 00:00:00').getTime();
 
-  return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
-}
+// Update the countdown every second
+const countdown = setInterval(() => {
+  // Get the current date and time
+  const now = new Date().getTime();
 
-function startCountdown(targetDate) {
-  const countdownElement = document.getElementById('countdown');
+  // Calculate the difference between the two dates
+  const difference = birthday - now;
 
-  function updateCountdown() {
-    countdownElement.textContent = calculateTimeDifference(targetDate);
+  // Calculate the days, hours, minutes, and seconds left
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  // Display the countdown on the webpage
+  document.querySelector('.countdown').innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+  // If the countdown is over, display a message
+  if (difference < 0) {
+    clearInterval(countdown);
+    document.querySelector('.countdown').innerHTML = "Happy Birthday to Me!";
   }
-
-  updateCountdown();
-  setInterval(updateCountdown, 1000);
-}
-
-// Replace the date with your birthday (format: YYYY, MM, DD)
-const myBirthday = new Date(2023, 11, 25);
-startCountdown(myBirthday);
+}, 1000);
